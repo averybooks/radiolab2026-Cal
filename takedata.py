3eb46e53c65107c7e1be1d9ff0e0beffa37b9678
Radio Pi <aboks104@gmail.com>
2026-02-03 00:08:29 -0800

    splitter data and fir attempts in takedata2

--- takedata.py
+++ takedata.py
@@ -1,16 +1,18 @@
 import ugradio
+from ugradio.sdr import SDR
 import pandas as pd
 from rtlsdr import RtlSdr
 import asyncio
 import time
 import numpy as np
 
-labelname = "noisedata1.npz"
-sdr = ugradio.sdr.SDR(sample_rate=3e6)
-data = sdr.capture_data(2048, nblocks=10)
+labelname = "bandpassdata_1.npz"
+                    sdr = ugradio.sdr.SDR(sample_rate=1e6)
+data = sdr.capture_data(2048,nblocks=10)
+print(sdr)
 print(data)
 np.savez(labelname, data)
 print("saved ", labelname)
 
-### np.load(filename.txt)
-#["arr_0"][0]
+#np.load(labelname.txt)
+["arr_0"][0]
---
+++ takedata2.py
@@ -0,0 +1,12 @@
+import ugradio
+import pandas as pd
+from rtlsdr import RtlSdr
+import asyncio
+import time
+import numpy as np
+
+sdr = ugradio.sdr.SDR(sample_rate=2.2e6, fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+data = sdr.capture_data(2048, nblocks=10)
+print(sdr)
+print(data)
+np.savez("fir_coeff_test.npz", data)
